de0c91df840c0441b4e8793a17d8ccc47f75dd5a
Andrés González <miroenlared@gmail.com>
2016-02-19 16:56:43 -0500

    ajuste de show and hidde de todos los niveles y de las tablas

--- js/angularScope.js
+++ js/angularScope.js
@@ -6,19 +6,64 @@ var nameSpace = angular.module("appScope", []);
 nameSpace.controller("appController", ['$scope','$http', function($scope, $http)
 		{
                         $scope.unit1 = 'one';
+                        $scope.unitA2 = 'one';
+                        $scope.unitA2plus = 'one';
+                        $scope.unitB1 = 'one';
+                        $scope.unitB1plus = 'one';
+                        $scope.showA1 = true;
+                        // botones de niveles
                         $scope.showBtnA1 = function() {
                                 $scope.showA1 = true;
                                 $scope.showA2 = false;
+                                $scope.showA2plus = false;
+                                $scope.showB1 = false;
+                                $scope.showB1plus = false;
                         };
                         $scope.showBtnA2 = function() {
                                 $scope.showA1 = false;
                                 $scope.showA2 = true;
+                                $scope.showA2plus = false;
+                                $scope.showB1 = false;
+                                $scope.showB1plus = false;
                         };
+                        $scope.showBtnA2plus = function() {
+                                $scope.showA1 = false;
+                                $scope.showA2 = false;
+                                $scope.showA2plus = true;
+                                $scope.showB1 = false;
+                                $scope.showB1plus = false;
+                        };
+
+                        $scope.showBtnB1 = function() {
+                                $scope.showA1 = false;
+                                $scope.showA2 = false;
+                                $scope.showA2plus = false;
+                                $scope.showB1 = true;
+                                $scope.showB1plus = false;
+                        };
+                        $scope.showBtnB1plus = function() {
+                                $scope.showA1 = false;
+                                $scope.showA2 = false;
+                                $scope.showA2plus = false;
+                                $scope.showB1 = false;
+                                $scope.showB1plus = true;
+                        };
+
+                        //
 			$http.get('../json/a1.json').success (function(data){
 				$scope.datos = data;
 		        });
 			$http.get('../json/a2.json').success (function(dataA2){
 				$scope.datosA2 = dataA2;
+		        });
+			$http.get('../json/a2plus.json').success (function(dataA2plus){
+				$scope.datosA2plus = dataA2plus;
+		        });
+			$http.get('../json/b1.json').success (function(dataB1){
+				$scope.datosB1 = dataB1;
+		        });
+			$http.get('../json/b1plus.json').success (function(dataB1plus){
+				$scope.datosB1plus = dataB1plus;
 		        });
 
 		}]
